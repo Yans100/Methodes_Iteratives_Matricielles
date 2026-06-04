23daf80f9ac228f90a877a4ca7b459911f361bbf
Yannick Poirier <122697698+Yans100@users.noreply.github.com>
2026-06-04 01:13:39 -0400

    Add files via upload

---
+++ src/Program.cs
@@ -0,0 +1,198 @@
+﻿using PIF1006_tp2;
+using System;
+
+//************************************************************************
+//*                                Yannick Poirier                       *
+//*                                 POIY04109403                         *
+//************************************************************************
+
+namespace PIF1006_tp2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double epsilon = 0.0001; // Valeur par défaut si l'utilisateur ne la change pas ou saisie une valeur inexistante
+
+            // Je créer localement une matrice ici. Possible de modifier pour utiliser un fichier JSON
+            Matrix2D MatriceUN = new Matrix2D("Première matrice", 3, 3); // on peut changer le nombre de lignes et colonnes de la matriceUN ici
+            Matrix2D MatriceDEUX = new Matrix2D("Deuxième matrice", 3, 3); // on peut changer le nombre de lignes et colonnes de la matriceDEUX ici
+
+            /* valeurs dans MatriceUN pour obtenir une convergence sans avoir de problème
+            MatriceUN.SetValue(0, 0, 4);
+            MatriceUN.SetValue(0, 1, 2);
+            MatriceUN.SetValue(0, 2, 0);
+            MatriceUN.SetValue(1, 0, 2);
+            MatriceUN.SetValue(1, 1, 10);
+            MatriceUN.SetValue(1, 2, 4);
+            MatriceUN.SetValue(2, 0, 0);
+            MatriceUN.SetValue(2, 1, 3);
+            MatriceUN.SetValue(2, 2, 15); */
+
+            /* valeurs dans matriceDEUX pour obtenir une convergence sans avoir de problème
+            MatriceDEUX.SetValue(0, 0, 2);
+            MatriceDEUX.SetValue(0, 1, 6);
+            MatriceDEUX.SetValue(0, 2, 10);
+            MatriceDEUX.SetValue(1, 0, 4);
+            MatriceDEUX.SetValue(1, 1, 8);
+            MatriceDEUX.SetValue(1, 2, 12);
+            MatriceDEUX.SetValue(2, 0, 6);
+            MatriceDEUX.SetValue(2, 1, 10);
+            MatriceDEUX.SetValue(2, 2, 18); */
+
+            // Autres valeurs pour MatriceUN (convergentes)
+            MatriceUN.SetValue(0, 0, 3);
+            MatriceUN.SetValue(0, 1, 1);
+            MatriceUN.SetValue(0, 2, 0);
+            MatriceUN.SetValue(1, 0, 1);
+            MatriceUN.SetValue(1, 1, 10);
+            MatriceUN.SetValue(1, 2, 2);
+            MatriceUN.SetValue(2, 0, 0);
+            MatriceUN.SetValue(2, 1, 2);
+            MatriceUN.SetValue(2, 2, 8); 
+
+            // Autres valeurs pour MatriceDEUX (convergentes)
+            MatriceDEUX.SetValue(0, 0, 6);
+            MatriceDEUX.SetValue(0, 1, 7);
+            MatriceDEUX.SetValue(0, 2, 15);
+            MatriceDEUX.SetValue(1, 0, 10);
+            MatriceDEUX.SetValue(1, 1, 11);
+            MatriceDEUX.SetValue(1, 2, 23);
+            MatriceDEUX.SetValue(2, 0, 7);
+            MatriceDEUX.SetValue(2, 1, 8);
+            MatriceDEUX.SetValue(2, 2, 17); 
+
+            System system = new System(MatriceUN, MatriceDEUX);
+
+            bool quitter = false;
+
+            // Ici commence la boucle principale du menu
+            while (!quitter)
+            {
+                // Affichage des options du menu
+                Console.WriteLine("Bienvenue dans le menu de l'utilisateur :");
+                Console.WriteLine("\n Merci de bien vouloir choisir une option :");
+                Console.WriteLine("1- Quitter le menu");
+                Console.WriteLine("2- Afficher les matrices");
+                Console.WriteLine("3- Résoudre avec Gauss-Seidel");
+                Console.WriteLine("4- Résoudre avec Jacobi");
+                Console.WriteLine("5- Définir la valeur de l'epsilon");
+                Console.WriteLine("6- Équation");
+
+                int choix;
+                if (!int.TryParse(Console.ReadLine(), out choix))
+                {
+                    Console.WriteLine("\nVeuillez saisir un numéro valide. Les options possibles sont inclusivement de 1 à 6.");
+                    continue;
+                }
+
+                switch (choix)
+                {
+                    case 1:
+                        // Quitter le menu
+                        quitter = true;
+                        break;
+
+                    // Afficher les matrices
+                    case 2:
+                        Console.WriteLine("Sélectionnez une matrice (1 ou 2) pour afficher:");
+                        int selectedMatrice;
+                        if (system.IsValid())
+                        {
+                            if (int.TryParse(Console.ReadLine(), out selectedMatrice) && (selectedMatrice == 1 || selectedMatrice == 2))
+                            {
+                                Console.WriteLine(selectedMatrice == 1 ? MatriceUN.ToString() : MatriceDEUX.ToString());
+                            }
+                            else
+                            {
+                                Console.WriteLine("Matrice non valide");
+                            }
+                        }
+                        break;
+
+                    case 3:
+                        // Résoudre avec Gauss-Seidel en prenant en compte l'epsilon
+                        if (system.IsValid())
+                        {
+                            Console.WriteLine("Veuillez entrer la valeur de l'epsilon pour Gauss-Seidel. Elle est à 0.0001 par défaut :");
+                            double epsilonGAUSS;
+                            if (!double.TryParse(Console.ReadLine(), out epsilonGAUSS))
+                            {
+                                epsilonGAUSS = 0.0001; // Valeur par défaut si la saisie n'est pas valide ou identique à la valeur par défaut
+                                Console.WriteLine("L'epsilon conserve sa valeur par défaut.");
+                            }
+
+                            Matrix2D matriceResolueGaussSeidel = system.SolveUsingGaussSeidel(epsilonGAUSS);
+
+                            // Afficher la solution obtenue
+                            AfficherSolution(matriceResolueGaussSeidel);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Le système n'est pas valide pour la résolution avec Gauss-Seidel");
+                        }
+                        break;
+
+                    case 4:
+                        // Résoudre avec Jacobi en prenant en compte l'epsilon
+                        if (system.IsValid())
+                        {
+                            Console.WriteLine("Entrez la valeur de l'epsilon (par défaut 0.0001) :");
+                            double epsilonJACOBI;
+                            if (!double.TryParse(Console.ReadLine(), out epsilonJACOBI))
+                            {
+                                epsilonJACOBI = 0.0001; // Valeur par défaut si la saisie est invalide ou identique à la valeur par défaut
+                                Console.WriteLine("L'epsilon conserve sa valeur par défaut.");
+                            }
+
+                            Matrix2D matriceResolueJacobi = system.SolveUsingJacobi(epsilonJACOBI);
+                            AfficherSolution(matriceResolueJacobi);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Le système n'est pas valide pour la résolution avec la méthode de Jacobi");
+                        }
+                        break;
+
+                    case 5:
+                        // Définir valeur epsilon
+                        Console.WriteLine("Veuillez entrer la valeur de l'epsilon :");
+                        if (!double.TryParse(Console.ReadLine(), out epsilon))
+                        {
+                            Console.WriteLine("L'epsilon reste à sa valeur par défaut.");
+                        }
+                        break;
+
+                    case 6:
+                        // Afficher l'équation
+                        string equation = system.GetEquation();
+                        if (!string.IsNullOrEmpty(equation))
+                        {
+                            Console.WriteLine(equation);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Impossible d'afficher l'équation.");
+                        }
+                        break;
+                }
+            }
+
+            // Méthode pour afficher la solution
+            static void AfficherSolution(Matrix2D solution)
+            {
+                if (solution != null)
+                {
+                    Console.WriteLine(solution);
+                }
+                else
+                {
+                    Console.WriteLine("Aucune solution trouvée");
+                }
+            }
+        }
+    }
+}
+
+
---
+++ src/System.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//************************************************************************
+//*                                Yannick Poirier                       *
+//*                                 POIY04109403                         *
+//************************************************************************
+
+namespace PIF1006_tp2
+{
+    public class System
+    {
+        public Matrix2D MatriceUN { get; private set; }
+        public Matrix2D MatriceDEUX { get; private set; }
+
+        public System(Matrix2D a, Matrix2D b)
+        {
+            MatriceUN = a;
+            MatriceDEUX = b;
+        }
+
+        // Permet de s'assurer que la matrice est conforme avant de faire des manipulations
+        public bool IsValid()
+        {
+            // Vérifier si MatriceUN est carrée
+            bool isSquare = MatriceUN.IsSquare();
+
+            // Vérifier si MatriceDEUX a le même nombre de lignes et colonnes que MatriceUN
+            bool isValidDEUX = MatriceDEUX.Matrice.GetLength(0) == MatriceUN.Matrice.GetLength(0) &&
+                               MatriceDEUX.Matrice.GetLength(1) == MatriceUN.Matrice.GetLength(1);
+
+            // Retourner vrai si MatriceUN est carrée et que MatriceDEUX a le même nombre de lignes et colonnes que MatriceUN
+            return isSquare && isValidDEUX;
+        }
+
+
+        // Méthode de résolution avec Gauss-Seidel
+        public Matrix2D SolveUsingGaussSeidel(double epsilon = 0.0001) // Valeur par défaut si l'utilisateur ne saisie pas de valeur d'epsilon (ou valeur invalide)
+        {
+            if (!IsValid())
+            {
+                throw new InvalidOperationException("Matrice incompatible pour la résolution en utilisant la méthode de Gauss-Seidel.");
+            }
+
+            // Initialisation de la solution de la matrice x
+            Matrix2D x = new Matrix2D("Solution Gauss-Seidel", MatriceUN.Matrice.GetLength(0), 1);
+
+            bool stopIteration = false;
+            int iterGAUSS = 0; // Compteur d'itérations
+
+            // Boucle pour les itérations
+            while (!stopIteration)
+            {
+                // Booleen pour vérifier si toutes les différences sont inférieures à la valeur d'epsilon
+                bool diffInferieur = true;
+
+                // Création d'une copie pour stocker les valeurs précédentes des inconnues
+                Matrix2D oldX = new Matrix2D("Solution précédente", x.Matrice.GetLength(0), 1);
+                Array.Copy(x.Matrice, oldX.Matrice, x.Matrice.Length);
+
+                // Boucle pour calculer les nouvelles valeurs des inconnues et les comparer avec les valeurs précédentes
+                for (int i = 0; i < MatriceUN.Matrice.GetLength(0); i++)
+                {
+                    double sum = 0.0;
+
+                    // Calcul de la somme pour la nouvelle itération
+                    for (int j = 0; j < MatriceUN.Matrice.GetLength(1); j++)
+                    {
+                        if (j != i)
+                        {
+                            sum += MatriceUN.Matrice[i, j] * x.Matrice[j, 0];
+                        }
+                    }
+
+                    // Mise à jour de la solution x[i]
+                    x.Matrice[i, 0] = (MatriceDEUX.Matrice[i, 0] - sum) / MatriceUN.Matrice[i, i];
+
+                    // Comparaison avec les valeurs précédentes pour vérifier la convergence
+                    double difference = Math.Abs(x.Matrice[i, 0] - oldX.Matrice[i, 0]);
+                    if (difference >= epsilon)
+                    {
+                        diffInferieur = false;
+                    }
+                }
+
+                // Affichage des valeurs des inconnues à chaque itération comme demandé
+                Console.WriteLine($"Iteration {iterGAUSS + 1}:");
+                for (int i = 0; i < x.Matrice.GetLength(0); i++)
+                {
+                    Console.WriteLine($"x{i + 1} = {x.Matrice[i, 0]}");
+                }
+                Console.WriteLine("-------------------------");
+
+                // Vérification si toutes les différences sont inférieures à epsilon
+                if (diffInferieur)
+                {
+                    stopIteration = true; // Arrêt des itérations si convergence
+                }
+
+                iterGAUSS++; // Incrémentation du compteur d'itérations
+            }
+
+            return x;
+        }
+
+        // Méthode de résolution de Jacobi
+        public Matrix2D SolveUsingJacobi(double epsilon = 0.0001) // Valeur de l'epsilon par défaut
+        {
+            if (!IsValid())
+            {
+                throw new InvalidOperationException("Matrice incompatible pour la résolution en utilisant la méthode de Jacobi.");
+            }
+
+            // Initialisation de la solution x
+            Matrix2D x = new Matrix2D("Solution Jacobi", MatriceUN.Matrice.GetLength(0), 1);
+            Matrix2D oldX = new Matrix2D("Solution précédente", x.Matrice.GetLength(0), 1);
+
+            bool stopIteration = false;
+            int iterJACOBI = 0; // Compteur d'itérations
+
+            // Boucle pour les itérations
+            while (!stopIteration)
+            {
+                // Création d'une copie pour stocker les valeurs précédentes des inconnues
+                Array.Copy(x.Matrice, oldX.Matrice, x.Matrice.Length);
+
+                // Boucle pour calculer les nouvelles valeurs des inconnues
+                for (int i = 0; i < MatriceUN.Matrice.GetLength(0); i++)
+                {
+                    double sum = 0.0;
+
+                    // Calcul de la somme pour la nouvelle itération (SANS utiliser les nouvelles valeurs)
+                    for (int j = 0; j < MatriceUN.Matrice.GetLength(1); j++)
+                    {
+                        if (j != i)
+                        {
+                            sum += MatriceUN.Matrice[i, j] * oldX.Matrice[j, 0];
+                        }
+                    }
+
+                    // Mise à jour de la solution x[i]
+                    x.Matrice[i, 0] = (MatriceDEUX.Matrice[i, 0] - sum) / MatriceUN.Matrice[i, i];
+                }
+
+                // Vérification de la convergence
+                bool diffInferieur = true;
+                for (int i = 0; i < x.Matrice.GetLength(0); i++)
+                {
+                    double difference = Math.Abs(x.Matrice[i, 0] - oldX.Matrice[i, 0]);
+                    if (difference >= epsilon)
+                    {
+                        diffInferieur = false;
+                        break; // Sort de la boucle dès qu'une différence est trop grande
+                    }
+                }
+
+                // Affichage des valeurs des inconnues à chaque itération comme pour Gauss
+                Console.WriteLine($"Iteration {iterJACOBI + 1}:");
+                for (int i = 0; i < x.Matrice.GetLength(0); i++)
+                {
+                    Console.WriteLine($"x{i + 1} = {x.Matrice[i, 0]}");
+                }
+                Console.WriteLine("-------------------------");
+
+                // Vérification si toutes les différences sont inférieures à epsilon
+                if (diffInferieur)
+                {
+                    stopIteration = true; // Arrêt des itérations si convergence
+                }
+
+                iterJACOBI++; // Incrémentation du compteur d'itérations
+            }
+
+            return x;
+        }
+
+        // Méthode pour équation
+        public string GetEquation()
+        {
+            return IsValid() ? ToStringEquation() : "Non valide pour l'affichage de l'équation";
+        }
+
+        private string ToStringEquation()
+        {
+            int rows = MatriceUN.Matrice.GetLength(0);
+            int cols = MatriceUN.Matrice.GetLength(1);
+
+            StringBuilder equationString = new();
+
+            // Parcourir les lignes de la matrice MatriceUN
+            for (int i = 0; i < rows; i++)
+            {
+                // Parcourir les colonnes de la matrice MatriceUN
+                for (int j = 0; j < cols - 1; j++)
+                {
+                    // Ajouter les termes des équations (sauf le dernier)
+                    equationString.Append($"{MatriceUN.Matrice[i, j].ToString("0.##")}x{j + 1} + ");
+                }
+
+                // Ajouter le dernier terme de l'équation avec le résultat de MatriceDEUX
+                int lastColIndex = cols - 1;
+                equationString.Append($"{MatriceUN.Matrice[i, lastColIndex]}x{lastColIndex + 1} = {MatriceDEUX.Matrice[i, 0]}\n");
+            }
+
+            return equationString.ToString();
+        }
+    }
+}
